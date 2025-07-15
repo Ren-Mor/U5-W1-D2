3f2911ae8ac28f7e45b168dd4daadec3e939e43d
Ren-Mor <renatomorelli@hotmail.it>
2025-07-15 16:39:45 +0200

    fix

--- src/main/java/com/epicode/U5D1/U5D1Application.java
+++ src/main/java/com/epicode/U5D1/U5D1Application.java
@@ -1,10 +1,8 @@
 package com.epicode.U5D1;
 
 import com.epicode.U5D1.entities.Menu;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 
